cb394800eaeec8853ca7e3b8118e69b5f0d94f1f
brianbianco <brian.bianco@gmail.com>
2016-04-14 15:35:11 -0400

    Removes erroneous comment

--- poyntstore.go
+++ poyntstore.go
@@ -2,12 +2,5 @@ package main
 
 type PoyntStore interface {
 	Write(key string, p Poynt) bool
-
-	/* Filter needs to implement the following predicates, they will be passed in
-	   as a URL query string
-	    op, op_lt, op_le, op_gt, op_ge
-	    obs, obs_lt, obs_le, obs_gt, obs_ge
-	    log, log_lt, log_le, log_gt, log_ge
-	*/
 	Filter(key string, f map[string][]string) []Poynt
 }
